eb97f41c6cec3edca197c43f8410301f5243d49c
IrinaM21 <56461226+IrinaM21@users.noreply.github.com>
2020-10-30 14:49:09 -0700

    Add annotation inpainting code
    Preprocesses data, creates mask/image pairs, trains model, and displays output of model

---
+++ annotation_inpainting.py
@@ -0,0 +1,404 @@
+
+# prepares image data and random masks and trains the inpainting model
+# !git clone https://github.com/ayulockin/deepimageinpainting.git
+
+# keras must be installed
+# opencv must be installed
+
+import torch
+import keras
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+import torchvision.transforms as transforms
+from torchvision import datasets, models
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
+import tensorflow as tf
+
+###################
+## PREPROCESSING ##
+###################
+
+class createAugment(keras.utils.Sequence):
+  # Generates masked_image, masks, and target images for training
+  def __init__(self, X, y, batch_size=20, dim=(224, 224), n_channels=3, shuffle=True):
+      # Initialize the constructor
+      self.batch_size = batch_size
+      self.X = X
+      self.y = y
+      self.dim = dim
+      self.n_channels = n_channels
+      self.shuffle = shuffle
+      self.on_epoch_end()
+
+  def __len__(self):
+    # Denotes the number of batches per epoch
+    return int(np.floor(len(self.X) / self.batch_size))
+
+  def __getitem__(self, index):
+    # Generate one batch of data
+    indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+    # Generate data
+    X_inputs, y_output = self.__data_generation(indexes)
+    return X_inputs, y_output
+
+  def on_epoch_end(self):
+    # Updates indexes after each epoch
+    self.indexes = np.arange(len(self.X))
+    if self.shuffle:
+      np.random.shuffle(self.indexes)
+
+  def __data_generation(self, idxs):
+    # Masked_images is a matrix of masked images used as input
+    Masked_images = np.empty((self.batch_size, self.dim[0], self.dim[1], self.n_channels)) # Masked image
+    # Mask_batch is a matrix of binary masks used as input
+    Mask_batch = np.empty((self.batch_size, self.dim[0], self.dim[1], self.n_channels)) # Binary Masks
+    # y_batch is a matrix of original images used for computing error from reconstructed image
+    y_batch = np.empty((self.batch_size, self.dim[0], self.dim[1], self.n_channels)) # Original image
+    
+
+    ## Iterate through random indexes
+    for i, idx in enumerate(idxs):
+      image_copy = self.X[idx].copy()
+  
+      ## Get mask associated to that image
+      masked_image, mask = self.__createMask(image_copy)
+      
+      Masked_images[i,] = masked_image
+      Mask_batch[i,] = mask
+      y_batch[i] = self.y[idx]
+
+    ## Return mask as well because partial convolution require the same.
+    return [Masked_images, Mask_batch], y_batch
+
+  def __createMask(self, img):
+    ## Prepare masking matrix
+    mask = np.full((224,224,3), 255, np.uint8) ## White background
+    for _ in range(np.random.randint(1, 10)):
+      # Get random x locations to start line
+      x1, x2 = np.random.randint(1, 224), np.random.randint(1, 224)
+      # Get random y locations to start line
+      y1, y2 = np.random.randint(1, 224), np.random.randint(1, 224)
+      # Get random thickness of the line drawn
+      thickness = np.random.randint(1, 3)
+      # Draw black line on the white mask
+      cv2.line(mask,(x1,y1),(x2,y2),(0,0,0),thickness)
+
+    ## Mask the image
+    masked_image = img.copy()
+    masked_image[mask==0] = 255
+
+    return masked_image, mask
+
+PATH = '/data/jedrzej/medical/covid_dataset/'
+
+# get all of the training CXRs and labels
+train = tf.keras.preprocessing.image_dataset_from_directory(
+    PATH,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(224, 224),
+    batch_size=20)
+
+# extracts training CXRs
+x_train = np.concatenate([x for x, y in train], axis=0)
+# extracts training labels
+y_train = np.concatenate([y for x, y in train], axis=0)
+
+# create masks for each CXR to be  used in training the inpainting model
+traingen = createAugment(x_train, x_train)
+
+# get all of the validation images and labels
+test = tf.keras.preprocessing.image_dataset_from_directory(
+   PATH,
+   validation_split=0.2,
+   subset="validation",
+   seed=123,
+   image_size=(224, 224),
+   batch_size=20)
+
+# extracts validation CXRs
+x_test = np.concatenate([x for x, y in test], axis=0)
+# extracts validation labels
+y_test = np.concatenate([y for x, y in test], axis=0)
+
+# create masks for each CXR to be used in validation
+testgen = createAugment(x_test, x_test, shuffle=False)
+
+# calculate the f1 score
+def dice_coef(y_true, y_pred):
+    y_true_f = keras.backend.flatten(y_true)
+    y_pred_f = keras.backend.flatten(y_pred)
+    intersection = keras.backend.sum(y_true_f * y_pred_f)
+    return (2. * intersection) / (keras.backend.sum(y_true_f + y_pred_f))
+
+###########
+## MODEL ##
+###########
+
+class InpaintingModel:
+  '''
+  Build UNET like model for image inpainting task.
+  '''
+  def prepare_model(self, input_size=(224,224,3)):
+    input_image = keras.layers.Input(input_size)
+    input_mask = keras.layers.Input(input_size, name='encoder_input')
+  
+    conv1, mask1, conv2, mask2 = self.__encoder_layer(224, input_image, input_mask, ['conv1', 'conv2'])
+    conv3, mask3, conv4, mask4 = self.__encoder_layer(448, conv2, mask2, ['conv3', 'conv4'])
+    conv5, mask5, conv6, mask6 = self.__encoder_layer(896, conv4, mask4, ['conv5', 'conv6'])
+    conv7, mask7, conv8, mask8 = self.__encoder_layer(1792, conv6, mask6, ['conv7', 'encoder_output'])
+
+    conv9, mask9, conv10, mask10 = self.__decoder_layer(1792, 896, conv8, mask8, conv7, mask7, ['conv9', 'conv10'])
+    conv11, mask11, conv12, mask12 = self.__decoder_layer(896, 448, conv10, mask10, conv5, mask5, ['conv11', 'conv12'])
+    conv13, mask13, conv14, mask14 = self.__decoder_layer(448, 224, conv12, mask12, conv3, mask3, ['conv13', 'conv14'])
+    conv15, mask15, conv16, mask16 = self.__decoder_layer(224, 3, conv14, mask14, conv1, mask1, ['conv15', 'decoder_output'])
+
+    outputs = keras.layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')(conv16)
+
+    return keras.models.Model(inputs=[input_image, input_mask], outputs=[outputs])
+    
+  def __encoder_layer(self, filters, in_layer, in_mask, names):
+    conv1, mask1 = PConv2D(224, (3,3), strides=1, padding='same', name=names[0])([in_layer, in_mask])
+    conv1 = keras.activations.relu(conv1)
+
+    conv2, mask2 = PConv2D(224, (3,3), strides=2, padding='same', name=names[1])([conv1, mask1])
+    # conv2 = keras.layers.BatchNormalization()(conv2, training=True)
+    conv2 = keras.activations.relu(conv2)
+
+    return conv1, mask1, conv2, mask2
+
+  def __decoder_layer(self, filter1, filter2, in_img, in_mask, share_img, share_mask, names):
+    up_img = keras.layers.UpSampling2D(size=(2,2))(in_img)
+    up_mask = keras.layers.UpSampling2D(size=(2,2))(in_mask)
+    concat_img = keras.layers.Concatenate(axis=3)([share_img, up_img])
+    concat_mask = keras.layers.Concatenate(axis=3)([share_mask, up_mask])
+
+    conv1, mask1 = PConv2D(filter1, (3,3), padding='same', name=names[0])([concat_img, concat_mask])
+    conv1 = keras.activations.relu(conv1)
+
+    conv2, mask2 = PConv2D(filter2, (3,3), padding='same', name=names[1])([conv1, mask1])
+    # conv2 = keras.layers.BatchNormalization()(conv2)
+    conv2 = keras.activations.relu(conv2)
+
+    return conv1, mask1, conv2, mask2
+
+# Reference: https://github.com/MathiasGruber/PConv-Keras/blob/master/libs/pconv_layer.py
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import InputSpec
+from tensorflow.keras.layers import Conv2D
+
+
+class PConv2D(Conv2D):
+    def __init__(self, *args, n_channels=3, mono=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.input_spec = [InputSpec(ndim=4), InputSpec(ndim=4)]
+
+    def build(self, input_shape):        
+        """Adapted from original _Conv() layer of Keras        
+        param input_shape: list of dimensions for [img, mask]
+        """
+        
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+            
+        if input_shape[0][channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs should be defined. Found `None`.')
+            
+        self.input_dim = input_shape[0][channel_axis]
+        
+        # Image kernel
+        kernel_shape = self.kernel_size + (self.input_dim, self.filters)
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='img_kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        # Mask kernel
+        self.kernel_mask = K.ones(shape=self.kernel_size + (self.input_dim, self.filters))
+
+        # Calculate padding size to achieve zero-padding
+        self.pconv_padding = (
+            (int((self.kernel_size[0]-1)/2), int((self.kernel_size[0]-1)/2)), 
+            (int((self.kernel_size[0]-1)/2), int((self.kernel_size[0]-1)/2)), 
+        )
+
+        # Window size - used for normalization
+        self.window_size = self.kernel_size[0] * self.kernel_size[1]
+        
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.built = True
+
+    def call(self, inputs, mask=None):
+        '''
+        We will be using the Keras conv2d method, and essentially we have
+        to do here is multiply the mask with the input X, before we apply the
+        convolutions. For the mask itself, we apply convolutions with all weights
+        set to 1.
+        Subsequently, we clip mask values to between 0 and 1
+        ''' 
+
+        # Both image and mask must be supplied
+        if type(inputs) is not list or len(inputs) != 2:
+            raise Exception('PartialConvolution2D must be called on a list of two tensors [img, mask]. Instead got: ' + str(inputs))
+
+        # Padding done explicitly so that padding becomes part of the masked partial convolution
+        images = K.spatial_2d_padding(inputs[0], self.pconv_padding, self.data_format)
+        masks = K.spatial_2d_padding(inputs[1], self.pconv_padding, self.data_format)
+
+        # Apply convolutions to mask
+        mask_output = K.conv2d(
+            masks, self.kernel_mask, 
+            strides=self.strides,
+            padding='valid',
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate
+        )
+
+        # Apply convolutions to image
+        img_output = K.conv2d(
+            (images*masks), self.kernel, 
+            strides=self.strides,
+            padding='valid',
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate
+        )        
+
+        # Calculate the mask ratio on each pixel in the output mask
+        mask_ratio = self.window_size / (mask_output + 1e-8)
+
+        # Clip output to be between 0 and 1
+        mask_output = K.clip(mask_output, 0, 1)
+
+        # Remove ratio values where there are holes
+        mask_ratio = mask_ratio * mask_output
+
+        # Normalize iamge output
+        img_output = img_output * mask_ratio
+
+        # Apply bias only to the image (if chosen to do so)
+        if self.use_bias:
+            img_output = K.bias_add(
+                img_output,
+                self.bias,
+                data_format=self.data_format)
+        
+        # Apply activations on the image
+        if self.activation is not None:
+            img_output = self.activation(img_output)
+            
+        return [img_output, mask_output]
+    
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_last':
+            space = input_shape[0][1:-1]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding='same',
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            new_shape = (input_shape[0][0],) + tuple(new_space) + (self.filters,)
+            return [new_shape, new_shape]
+        if self.data_format == 'channels_first':
+            space = input_shape[2:]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding='same',
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            new_shape = (input_shape[0], self.filters) + tuple(new_space)
+            return [new_shape, new_shape]
+
+## Reference: https://github.com/keras-team/keras/blob/7a39b6c62d43c25472b2c2476bd2a8983ae4f682/keras/utils/conv_utils.py#L85
+def conv_output_length(input_length, filter_size,
+                       padding, stride, dilation=1):
+    """Determines output length of a convolution given input length.
+    # Arguments
+        input_length: integer.
+        filter_size: integer.
+        padding: one of `"same"`, `"valid"`, `"full"`.
+        stride: integer.
+        dilation: dilation rate, integer.
+    # Returns
+        The output length (integer).
+    """
+    if input_length is None:
+        return None
+    assert padding in {'same', 'valid', 'full', 'causal'}
+    dilated_filter_size = (filter_size - 1) * dilation + 1
+    if padding == 'same':
+        output_length = input_length
+    elif padding == 'valid':
+        output_length = input_length - dilated_filter_size + 1
+    elif padding == 'causal':
+        output_length = input_length
+    elif padding == 'full':
+        output_length = input_length + dilated_filter_size - 1
+    return (output_length + stride - 1) // stride
+
+##############
+## TRAINING ##
+##############
+
+# creating the model
+import os
+import cv2
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+model = InpaintingModel().prepare_model()
+model.compile(optimizer='adam', loss='mean_absolute_error', metrics=[dice_coef])
+
+# plots the model. requires sudo to be installed in server
+#keras.utils.plot_model(model, show_shapes=True, dpi=60, to_file='model_v2.png')
+
+# training the model
+_ = model.fit_generator(traingen, validation_data=testgen, 
+          epochs=2, 
+          steps_per_epoch=len(traingen), 
+          validation_steps=len(testgen),
+          use_multiprocessing=True
+          )
+
+# Legend: Original Image | Mask generated | Inpainted Image | Ground Truth
+
+# Examples
+rows = 32
+sample_idx = 54
+[masked_images, masks], sample_labels = testgen[sample_idx]
+
+fig, axs = plt.subplots(nrows=rows, ncols=4, figsize=(8, 2*rows))
+
+for i in range(32):
+  inputs = [masked_images[i].reshape((1,)+masked_images[i].shape), masks[i].reshape((1,)+masks[i].shape)]
+  impainted_image = model.predict(inputs)
+  axs[i][0].imshow(masked_images[i])
+  axs[i][1].imshow(masks[i])
+  axs[i][2].imshow(impainted_image.reshape(impainted_image.shape[1:]))
+  axs[i][3].imshow(sample_labels[i])
+  
+plt.show()
