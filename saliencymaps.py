6274628b991d20807773f26436c6950ebfcdf215
Irina Malyugina <56461226+IrinaM21@users.noreply.github.com>
2021-02-17 11:28:08 -0800

    Update saliencymaps.py

--- saliencymaps.py
+++ saliencymaps.py
@@ -1,13 +1,18 @@
+# visualizes salient features in image using backpropagation, guided backprop
+
+
 # flashtorch and torch 1.6 need to be installed
+import torch
 from flashtorch.saliency import Backprop
 from flashtorch.utils import load_image, apply_transforms, denormalize, format_for_plotting
 
-PATH = 'FILLER PATH'
+PATH = 'IMAGE PATH'
 input = apply_transforms(load_image(PATH))
+
 # COVID-19 is 0, Normal is 1, Viral Pneumonia is 2
 target_class = 0
 
-MODEL = 'FILLER PATH'
+MODEL = 'MODEL PATH'
 model = torch.load(MODEL)
 
 backprop = Backprop(model)
@@ -18,5 +23,5 @@ backprop.visualize(input, target_class)
 
 guided_gradients = backprop.calculate_gradients(input, target_class, guided=True)
 max_guided_gradients = backprop.calculate_gradients(input, target_class, take_max=True, guided=True)
-# visualize
+
 backprop.visualize(input, target_class)
