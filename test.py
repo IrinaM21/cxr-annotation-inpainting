d70c3da718d973d1dc1cf405867664fd3ae3b738
Irina Malyugina <56461226+IrinaM21@users.noreply.github.com>
2021-04-02 13:22:46 -0700

    Add test.py, train.py

---
+++ test.py
@@ -0,0 +1,24 @@
+from options import test_options
+from dataloader import data_loader
+from model import create_model
+from util import visualizer
+from itertools import islice
+
+if __name__=='__main__':
+    # get testing options
+    opt = test_options.TestOptions().parse()
+    # creat a dataset
+    dataset = data_loader.dataloader(opt)
+    dataset_size = len(dataset) * opt.batchSize
+    print('testing images = %d' % dataset_size)
+    # create a model
+    model = create_model(opt)
+    print("created model")
+    model.eval()
+    print("prepared model for evaluation")
+    # create a visualizer
+    visualizer = visualizer.Visualizer(opt)
+    print("starting evaluation")
+    for i, data in enumerate(islice(dataset, opt.how_many)):
+        model.set_input(data)
+        model.test()
