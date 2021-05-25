8652021cf1ad0d6ada1d2b4c182e80b8142b9667
Irina Malyugina <56461226+IrinaM21@users.noreply.github.com>
2021-05-24 22:06:37 -0700

    Update test.py

--- test.py
+++ test.py
@@ -22,6 +22,6 @@ if __name__=='__main__':
     print("starting evaluation")
     # for i, data in enumerate(islice(dataset, opt.how_many)):
     for i, data in enumerate(dataset):
-        print(data)
+        print(data.size())
         model.set_input(data)
         model.test()
